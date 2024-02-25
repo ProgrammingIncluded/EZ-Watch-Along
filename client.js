818a8e62b6bbcd2a8c5fca27da31aa5b46bdc37d
Timothy Yoon <timsyoon@gmail.com>
2024-02-24 18:02:22 -0600

    Reduce sidebar width; adjust how truncated file names are displayed

--- client.js
+++ client.js
@@ -112,16 +112,6 @@ function side_bar_search() {
 
     let idx = 0;
     side_bar.html("");
-    let clean_name_display = (fname) => {
-        let tokens = fname.split("/");
-        let last = tokens[tokens.length - 1];
-        let max_characters = 40;
-
-        if (last.length <= max_characters)
-            return last;
-
-        return last.substring(0, 20) + "..." + last.substring(last.length - max_characters, last.length);
-    }
     for (let r of filtered_results) {
         let fname_dom = $("<div>", {"class": "search_pathname", "fname": r.fname})
         fname_dom.html(clean_name_display(r.fname));
@@ -167,6 +157,25 @@ function side_bar_search() {
 
     });
 };
+
+function clean_name_display (fname) {
+    let tokens = fname.split("/");
+    let last = tokens[tokens.length - 1];
+    let max_characters = 40;
+
+    if (last.length <= max_characters)
+        return last;
+
+    let ellipsis = "...";
+    let pre_ellipsis_length = Math.floor(max_characters / 2);
+    let post_ellipsis_length = max_characters - pre_ellipsis_length - ellipsis.length;
+
+    let clean_name = (last.substring(0, pre_ellipsis_length) +
+                      ellipsis +
+                      last.substring(last.length - post_ellipsis_length, last.length));
+
+    return clean_name;
+}
 
 $(document).ready(function() {
     // Set a update loop to sync with server.
